22fed2b5cf067a6d6faa64cdfd45dc0b668c3a0e
tank <347477499@qq.com>
2019-12-26 22:01:00 +0800

    add .gitignore

---
+++ CompanyCodingTest/src/test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @author tank
+ * @create 2019/10/30 15:50
+ */
+public class test {
+    public static void main(String[] args) {
+        //todo
+        Scanner sc = new Scanner(System.in);
+        int nextInt = sc.nextInt();
+        switch (nextInt) {
+
+        }
+    }
+    public enum state{
+        dsStart1,
+        dsStart2
+    }
+}
