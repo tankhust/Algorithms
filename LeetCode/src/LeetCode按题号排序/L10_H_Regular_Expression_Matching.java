999fb37d5557d7f69de4114da2e259e7e49b59d9
tank <347477499@qq.com>
2020-02-19 11:31:50 +0800

    leetcode11

---
+++ LeetCode/src/LeetCode按题号排序/L10_H_Regular_Expression_Matching.java
@@ -0,0 +1,62 @@
+package LeetCode按题号排序;
+
+/**
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * Note:
+ * <p>
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ * <p>
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * Example 4:
+ * <p>
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+ * Example 5:
+ * <p>
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ *
+ * @author tank
+ * @create 2020/02/19 11:12
+ */
+public class L10_H_Regular_Expression_Matching {
+    public static void main(String[] args) {
+        L10_H_Regular_Expression_Matching l10 = new L10_H_Regular_Expression_Matching();
+        String s = "aa", p = "a";
+        System.out.println(l10.isMatch(s, p));
+    }
+
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
---
+++ LeetCode/src/LeetCode按题号排序/L11_Container_With_Most_Water.java
@@ -0,0 +1,48 @@
+package LeetCode按题号排序;
+
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ * <p>
+ * Note: You may not slant the container and n is at least 2.
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * <p>
+ * <p>
+ * <p>
+ * Example:
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ *
+ * @author tank
+ * @create 2020/02/19 11:15
+ */
+public class L11_Container_With_Most_Water {
+    public static void main(String[] args) {
+        L11_Container_With_Most_Water l11 = new L11_Container_With_Most_Water();
+        int[] arr = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(l11.maxArea(arr));
+    }
+
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) {
+            return 0;
+        }
+        int len = height.length;
+        int l = 0, r = len - 1;
+        int max = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                max = Math.max((r - l) * height[l], max);
+                l++;
+            } else {
+                max = Math.max((r - l) * height[r], max);
+                r--;
+            }
+        }
+        return max;
+    }
+}
